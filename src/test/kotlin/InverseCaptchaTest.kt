e6241a713f8ef4881333a0d59e5b9ffeb936babb
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2020-11-29 00:17:34 +0100

    automated test for day1From2017

--- src/test/kotlin/InverseCaptchaTest.kt
+++ src/test/kotlin/InverseCaptchaTest.kt
@@ -1,5 +1,3 @@
-package test
-import inverseCaptcha
 import org.junit.Test
 import kotlin.test.assertEquals
 
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+  @Test
+  fun day1From2017_works() {
+    assertEquals(1141, day1From2017())
+  }
+}
