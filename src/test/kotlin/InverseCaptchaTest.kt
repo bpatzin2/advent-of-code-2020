a9fc62c9e62622afc4732af5477cdf91cd8e167d
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2020-11-29 00:24:50 +0100

    fix test code format

--- src/test/kotlin/InverseCaptchaTest.kt
+++ src/test/kotlin/InverseCaptchaTest.kt
@@ -2,28 +2,28 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class InverseCaptchaTest {
-    @Test
-    fun whenEmptyList_thenAnswerIs0() {
-        assertEquals(0, inverseCaptcha(listOf()))
-    }
+  @Test
+  fun whenEmptyList_thenAnswerIs0() {
+    assertEquals(0, inverseCaptcha(listOf()))
+  }
 
-    @Test
-    fun whenNoMatchesInList_thenAnswerIs0() {
-        assertEquals(0, inverseCaptcha(listOf(1,2)))
-    }
+  @Test
+  fun whenNoMatchesInList_thenAnswerIs0() {
+    assertEquals(0, inverseCaptcha(listOf(1,2)))
+  }
 
-    @Test
-    fun whenSingleElementList_thenAnswerIsThatElement() {
-        assertEquals(1, inverseCaptcha(listOf(1)))
-    }
+  @Test
+  fun whenSingleElementList_thenAnswerIsThatElement() {
+    assertEquals(1, inverseCaptcha(listOf(1)))
+  }
 
-    @Test
-    fun whenListHasOnePair_thenAnswerIsElementValue() {
-        assertEquals(1, inverseCaptcha(listOf(1,1,0)))
-    }
+  @Test
+  fun whenListHasOnePair_thenAnswerIsElementValue() {
+    assertEquals(1, inverseCaptcha(listOf(1,1,0)))
+  }
 
-    @Test
-    fun whenListHasOnePairAndEndMatchesStart_thenAnswerIsPairElementPlusStartValue() {
-        assertEquals(3, inverseCaptcha(listOf(2,1,1,2)))
-    }
+  @Test
+  fun whenListHasOnePairAndEndMatchesStart_thenAnswerIsPairElementPlusStartValue() {
+    assertEquals(3, inverseCaptcha(listOf(2,1,1,2)))
+  }
 }
