a423b04b0861f16d0d49f66931320235002ad723
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2020-12-01 07:38:23 +0100

    day1 pt1 works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,14 @@ fun day1From2017(): Long {
   return inverseCaptcha(numberList)
 }
 
-fun day1pt1(): Long {
-  val numberList = asNumberList("input/test0.txt")
-  return 0L;
+fun day1pt1(): Int {
+  return day1pt1("input/day1.txt")
+}
+
+fun day1pt1(pathname: String): Int {
+  val numberList = asNumberListByNewLine(pathname)
+  val pair = find(numberList, 2020) ?: return -1
+  return pair.first * pair.second
 }
 
 fun main(){
