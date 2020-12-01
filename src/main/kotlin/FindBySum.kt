da987ec72e04d9be1ccfc55618c7b5ec5f5db265
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2020-12-01 08:23:34 +0100

    day1 pt2 passing tests

--- src/main/kotlin/FindBySum.kt
+++ src/main/kotlin/FindBySum.kt
@@ -1,10 +1,35 @@
-fun findPair(numberList: List<Int>, sum: Int): Pair<Int, Int>? {
+// 0(N^2) implementation to find triplet that sums to given value
+fun findTriplet(numberList: List<Int>, sum: Int): List<Int>? {
     val numberCounts = numberList.groupingBy { it }.eachCount()
     for (num in numberList) {
+        val updatedCounts = numberCounts.toMutableMap()
+        val currCount = updatedCounts[num]!!
+        updatedCounts[num] = currCount-1
+        val updatedSum = sum - num
+        val pair = findPair(updatedSum, updatedCounts)
+        if(pair != null){
+            return listOf(num, pair.first, pair.second)
+        }
+    }
+    return null
+}
+
+fun findPair(numberList: List<Int>, sum: Int): Pair<Int, Int>? {
+    val numberCounts = numberList.groupingBy { it }.eachCount()
+    return findPair(sum, numberCounts)
+}
+
+// 0(N) implementation to find pair that sums to given value
+private fun findPair(
+    sum: Int,
+    numberCounts: Map<Int, Int>,
+): Pair<Int, Int>? {
+    val numList = numberCounts.keys.filter { key -> numberCounts[key]!! > 0 }
+    for (num in numList) {
         val expectedOther = sum - num
-        val expectedCountOfOther = if(num == expectedOther) 2 else 1
+        val expectedCountOfOther = if (num == expectedOther) 2 else 1
         val actualOtherCount = numberCounts.getOrDefault(expectedOther, 0)
-        if(actualOtherCount >= expectedCountOfOther){
+        if (actualOtherCount >= expectedCountOfOther) {
             return Pair(num, expectedOther)
         }
     }
--- src/test/kotlin/FindBySumTest.kt
+++ src/test/kotlin/FindBySumTest.kt
@@ -4,28 +4,55 @@ import kotlin.test.fail
 
 class FindBySumTest {
   @Test
-  fun whenEmptyList_thenAnswerIsNull() {
+  fun findPair_whenEmptyList_thenAnswerIsNull() {
     assertEquals(null, findPair(listOf(), 42))
   }
 
   @Test
-  fun whenSize1List_thenAnswerIsNull() {
+  fun findPair_whenSize1List_thenAnswerIsNull() {
     assertEquals(null, findPair(listOf(1), 42))
   }
 
   @Test
-  fun whenThereIsNoPairThatSums_thenAnswerIsNull() {
+  fun findPair_whenThereIsNoPairThatSums_thenAnswerIsNull() {
     assertEquals(null, findPair(listOf(1, 2), 42))
   }
 
   @Test
-  fun whenThereIsAPairThatSums_thenAnswerIsThatPair() {
+  fun findPair_checkForHalf_thenAnswerIsNull() {
+    assertEquals(null, findPair(listOf(21, 2, 3), 42))
+  }
+
+  @Test
+  fun findPair_whenThereIsAPairThatSums_thenAnswerIsThatPair() {
     val expected: Set<Int> = setOf(2, 40)
-    val actualPair = findPair(listOf(1, 2, 3, 40), 42)
-    if(actualPair == null){
-      fail("Unexpected null result")
-    }
+    val actualPair = findPair(listOf(1, 2, 3, 40), 42)!!
     val actualSet: Set<Int> = actualPair.toList().toSet()
     assertEquals(expected, actualSet)
+  }
+
+  @Test
+  fun findTriplet_whenEmptyList_thenAnswerIsNull() {
+    assertEquals(null, findTriplet(listOf(), 42))
+  }
+
+  @Test
+  fun findTriplet_whenSize2List_thenAnswerIsNull() {
+    assertEquals(null, findTriplet(listOf(1, 41), 42))
+  }
+
+  @Test
+  fun findTriplet_whenThereIsNoTripletThatSums_thenAnswerIsNull() {
+    assertEquals(null, findTriplet(listOf(1, 2, 41), 42))
+  }
+
+  @Test
+  fun findTriplet_whenThereATripletThatSums_thenAnswerThatTriplet() {
+    assertEqualContent(listOf(1, 2, 39), findTriplet(listOf(1, 2, 39), 42)!!)
+  }
+
+  private fun <T>assertEqualContent(l1: List<T>, l2: List<T>){
+    assert(l1.containsAll(l2))
+    assert(l2.containsAll(l1))
   }
 }
