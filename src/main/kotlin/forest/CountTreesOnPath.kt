c9db81ff11202c0a52a0c304ad17e5cfe95439c1
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2020-12-03 07:56:33 +0100

    spacing cleanup

--- src/main/kotlin/forest/CountTreesOnPath.kt
+++ src/main/kotlin/forest/CountTreesOnPath.kt
@@ -5,17 +5,17 @@ const val TREE = '#'
 fun countTreesOnPath(
     forest: List<List<Char>>,
     pathRight: Int,
-    pathDown:Int,
+    pathDown: Int,
 ): Int{
-    var treeCount = 0
-    var xPos = 0
-    for (rowNum in forest.indices step pathDown) {
-        val row = forest[rowNum]
-        val locInRow = row[xPos]
-          if(locInRow == TREE)  {
-              treeCount++
-          }
-        xPos = (xPos + pathRight) % row.size
+  var treeCount = 0
+  var xPos = 0
+  for (rowNum in forest.indices step pathDown) {
+    val row = forest[rowNum]
+    val locInRow = row[xPos]
+    if(locInRow == TREE)  {
+      treeCount++
     }
-    return treeCount
+    xPos = (xPos + pathRight) % row.size
+  }
+  return treeCount
 }
