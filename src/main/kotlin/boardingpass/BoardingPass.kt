12ba4cfdfebb2b95b10d59f6009b38a5f1f4568b
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2020-12-05 07:34:57 +0100

    extract bspRange

--- src/main/kotlin/boardingpass/BoardingPass.kt
+++ src/main/kotlin/boardingpass/BoardingPass.kt
@@ -1,43 +1,12 @@
 package boardingpass
 
-import java.lang.RuntimeException
-
 const val NUM_ROWS = 128
 const val NUM_COLS = 8
 
 data class BoardingPass(val rowBsp: String, val columnBsp: String){
-
-  fun seatId(): Int{
-    return rowNumber() * 8 + colNumber()
-  }
-
-  fun rowNumber(): Int{
-    var range = 0 until NUM_ROWS
-    for(frontBack in rowBsp){
-      range = if(frontBack == 'F'){
-        firstHalf(range)
-      }else {
-        secondHalf(range)
-      }
-    }
-    return (range.first + range.last) / 2
-  }
-
-  fun colNumber(): Int{
-    var range = 0 until NUM_COLS
-    for(rightLeft in columnBsp){
-      range = if(rightLeft == 'L'){
-        firstHalf(range)
-      }else {
-        secondHalf(range)
-      }
-    }
-    return (range.first + range.last) / 2
-  }
-
-  private fun firstHalf(range: IntRange) = range.first .. (range.last - (range.count() / 2))
-  private fun secondHalf(range: IntRange) = (range.first + (range.count() / 2)) .. range.last
-
+  fun seatId() = rowNumber() * 8 + colNumber()
+  fun rowNumber() = bspRange(0 until NUM_ROWS, rowBsp, 'F')
+  fun colNumber() = bspRange(0 until NUM_COLS, columnBsp, 'L')
 }
 
 //FBFBBFFRLR
